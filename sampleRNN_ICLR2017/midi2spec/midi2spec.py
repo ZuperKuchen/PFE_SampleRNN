cbab9a980a35dd9aec76b367f747c73116ad70ea
Maxime Poret <mporet@emi.u-bordeaux1.fr>
2019-02-08 17:47:15 +0100

    Construction de la matrice de notes, non-testé

--- sampleRNN_ICLR2017/midi2spec/midi2spec.py
+++ sampleRNN_ICLR2017/midi2spec/midi2spec.py
@@ -1,7 +1,8 @@
 import midi
+import numpy as np
 
 def pitch_to_fonda (pitch):
-    return pitch
+    return np.round(27.5 * np.power(2.0,((pitch - 21.0)/12.0)))
 
 def open_midi (file_name):
     notes_pattern = []
@@ -12,6 +13,22 @@ def open_midi (file_name):
                 notes_pattern.append(event)
     return notes_pattern
 
+def get_max_tick(pattern):
+    t = 0
+    for event in pattern:
+        if(event.tick > t):
+            t = event.tick
+    return t
+
+def midi_matrix (pattern):
+    velocity_mat = np.zeros((128,get_max_tick(pattern)+1))
+    for event in pattern:
+        velocity_mat[event.pitch][event.tick] = event.velocity
+    return velocity_mat
+
 #main
 pattern = open_midi("mary.mid")
-print pattern
+for note_event in pattern:
+    print (pitch_to_fonda(note_event.pitch), note_event.tick, note_event.velocity)
+
+print midi_matrix(pattern).shape
