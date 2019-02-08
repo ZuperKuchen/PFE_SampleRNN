356d709a145a52f41d06863a21c3bda486c5bd1d
Maxime Poret <mporet@emi.u-bordeaux1.fr>
2019-02-08 17:23:22 +0100

    open_midi récupère et renvoie le tableau des note_events

--- sampleRNN_ICLR2017/midi2spec/midi2spec.py
+++ sampleRNN_ICLR2017/midi2spec/midi2spec.py
@@ -4,7 +4,14 @@ def pitch_to_fonda (pitch):
     return pitch
 
 def open_midi (file_name):
-    return midi.read_midifile(file_name)
+    notes_pattern = []
+    pattern = midi.read_midifile(file_name)
+    for track in pattern:
+        for event in track:
+            if type(event) == midi.events.NoteOnEvent or type(event) == midi.events.NoteOffEvent:
+                notes_pattern.append(event)
+    return notes_pattern
 
 #main
-print open_midi("mary.mid")
+pattern = open_midi("mary.mid")
+print pattern
