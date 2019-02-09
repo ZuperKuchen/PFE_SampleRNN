47c6ecf0f7584c3101b9fcff71cc3f531fbfae6c
Maxime Poret <mporet@emi.u-bordeaux1.fr>
2019-02-09 13:50:04 +0100

    Corrigé la fonction qui construit la matrice de velocity. Commencé la fonction qui construit le spectre

--- sampleRNN_ICLR2017/midi2spec/midi2spec.py
+++ sampleRNN_ICLR2017/midi2spec/midi2spec.py
@@ -1,12 +1,18 @@
 import midi
 import numpy as np
+import sys
 
-def pitch_to_fonda (pitch):
-    return np.round(27.5 * np.power(2.0,((pitch - 21.0)/12.0)))
+sample_rate = 44100
+
+def pitch_to_funda (pitch):
+    return 27.5 * np.power(2.0,((pitch - 21.0)/12.0))
 
 def open_midi (file_name):
     notes_pattern = []
+    
     pattern = midi.read_midifile(file_name)
+    pattern.make_ticks_abs()
+    
     for track in pattern:
         for event in track:
             if type(event) == midi.events.NoteOnEvent or type(event) == midi.events.NoteOffEvent:
@@ -20,15 +26,29 @@ def get_max_tick(pattern):
             t = event.tick
     return t
 
-def midi_matrix (pattern):
-    velocity_mat = np.zeros((128,get_max_tick(pattern)+1))
+def get_midi_matrix (pattern):
+    end_tick = get_max_tick(pattern)
+    midi_matrix = np.zeros((128,end_tick+1))
     for event in pattern:
-        velocity_mat[event.pitch][event.tick] = event.velocity
-    return velocity_mat
+        for i in range(event.tick, end_tick):
+            midi_matrix[event.pitch][i] = event.velocity
+    return midi_matrix
+
+def midi_to_spec (midi_matrix):
+    nb_ticks = (midi_matrix.shape)[1]
+    for tick in range(0, nb_ticks):
+        for pitch in range(0,127):
+            if midi_matrix[pitch][tick] != 0:
+                print 'TODO'
+        
 
 #main
-pattern = open_midi("mary.mid")
-for note_event in pattern:
-    print (pitch_to_fonda(note_event.pitch), note_event.tick, note_event.velocity)
+if __name__ == '__main__':
+    path = sys.argv[1]
+    pattern = open_midi(path)
+    for note_event in pattern:
+        print (note_event.tick, note_event.pitch, note_event.velocity, pitch_to_funda(note_event.pitch))
+    midi_matrix = get_midi_matrix(pattern)
+    midi_to_spec(midi_matrix)
 
-print midi_matrix(pattern).shape
+
