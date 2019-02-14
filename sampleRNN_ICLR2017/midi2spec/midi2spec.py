2bd35146553560de6e49fb14579188a1383be306
Maxime Poret <mporet@emi.u-bordeaux1.fr>
2019-02-14 16:14:22 +0100

    Modifié les arguments des fonctions qui nécessitent sample_rate et frame_size, pour pouvoir choisir ces paramètres quand on les appelle, plutôt que d'utiliser les valeurs globales par défaut. Raccourci la fonction get_max_tick, qui n'avait pas besoin de parcourir tous les events vu que les ticks sont ordonnés et absolus

--- sampleRNN_ICLR2017/midi2spec/midi2spec.py
+++ sampleRNN_ICLR2017/midi2spec/midi2spec.py
@@ -3,10 +3,10 @@ import numpy as np
 import numpy.fft as fft
 import wave
 
-#Fixed the SAMPLE_RATE at twice the frequency of the max MIDI pitch
-SAMPLE_RATE = 8372
-#FRAME_SIZE has to be the same as SAMPLE_RATE to accurately transcribe the frequencies
-FRAME_SIZE = 8372
+#Fixed the DEFAULT_SAMPLE_RATE at twice the frequency of the max MIDI pitch
+DEFAULT_SAMPLE_RATE = 8372
+#DEFAULT_FRAME_SIZE has to be the same as DEFAULT_SAMPLE_RATE to accurately transcribe the frequencies
+DEFAULT_FRAME_SIZE = 8372
 
 MIDI_MIN_PITCH = 21
 MIDI_MAX_PITCH = 108
@@ -16,8 +16,10 @@ def pitch_to_funda (pitch):
     return 27.5 * np.power(2.0,((pitch - 21.0)/12.0))
 
 #returns the index of the given frequency in a spectral frame
-def freq_to_index (freq):
-    return int(np.round(float(FRAME_SIZE) * freq / float(SAMPLE_RATE)))
+#fs = frame size
+#sr = sample rate
+def freq_to_index (freq, fs, sr):
+    return int(np.round(float(fs) * freq / float(sr)))
 
 #opens a midi file and returns an array of its note on/offsets
 def open_midi (file_name):
@@ -38,11 +40,7 @@ def open_midi (file_name):
 
 #returns the number of the last tick in the track (in absolute)
 def get_max_tick(pattern):
-    t = 0
-    for event in pattern:
-        if(event.tick > t):
-            t = event.tick
-    return t
+    return pattern[len(pattern)-1].tick
 
 #Builds the "piano roll" of the track : a matrix that contains the activation (velocity)
 #of each pitch for each tick
@@ -75,33 +73,36 @@ def get_frames_per_tick():
 
 #turns the piano roll into an amplitude spectrum of the corresponding fundamental
 #frequencies
-def midi_to_spec (piano_roll):
+#fs = frame size
+#sr = sample rate
+def midi_to_spec (piano_roll, fs, sr):
     nb_ticks = (piano_roll.shape)[1]
     frames_per_tick = get_frames_per_tick()
-    spectrum = np.zeros((frames_per_tick * nb_ticks, FRAME_SIZE), dtype=np.float)
+    spectrum = np.zeros((frames_per_tick * nb_ticks, fs), dtype=np.float)
 
     #transform each tick of the track into a spectral slice (frame)
     #that represents the fundamental frequencies of the pitches
     for tick in range(0, nb_ticks):
         for pitch in range(MIDI_MIN_PITCH, MIDI_MAX_PITCH):
             if piano_roll[pitch][tick] > 0 :
-                freq_index = freq_to_index(pitch_to_funda(pitch))
+                freq_index = freq_to_index(pitch_to_funda(pitch), fs, sr)
                 for i in range (0, frames_per_tick):
                     #amplitude computed from the velocity of the note
                     amp = float(piano_roll[pitch][tick])/128.0
                     #keep the symmetry of the spectrum
                     spectrum[tick * frames_per_tick + i][freq_index] = amp
-                    spectrum[tick * frames_per_tick + i][FRAME_SIZE-freq_index] = amp
+                    spectrum[tick * frames_per_tick + i][fs - freq_index] = amp
     return spectrum
 
 #For testing purposes : turns the spectrum into a listenable wav file
 #NOTE : the wav file will be very long, as each frame is one second in length
-def spec_to_wav (spectrum):
+#sr = sample rate
+def spec_to_wav (spectrum, sr):
     #open a new wav file and set its parameter
     wav_file = wave.open("out.wav", 'wb')
     wav_file.setnchannels(1)
     wav_file.setsampwidth(2)
-    wav_file.setframerate(SAMPLE_RATE)
+    wav_file.setframerate(sr)
     wav_file.setnframes(spectrum.shape[0])
 
     for frame in spectrum:
@@ -122,11 +123,11 @@ if __name__ == '__main__':
     #create the piano roll of the track
     piano_roll = get_piano_roll(pattern)
     #create the amplitude spectrum for that piano roll
-    spectrum = midi_to_spec(piano_roll)
+    spectrum = midi_to_spec(piano_roll, DEFAULT_FRAME_SIZE, DEFAULT_SAMPLE_RATE)
 
     #create the listenable wav file, to check that the melody can be recognized
     #from the spectrum
-    spec_to_wav(spectrum)
+    spec_to_wav(spectrum, DEFAULT_SAMPLE_RATE)
 
     #plot stuff
     import matplotlib.pyplot as plt
