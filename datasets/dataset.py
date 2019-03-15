e03f03173af4cbb8d2404752303b72b400522dd7
Maxime Poret <mporet@emi.u-bordeaux1.fr>
2019-03-15 18:59:36 +0100

    Première étape factorisation dataset.py : spearation wav et midi dans leurs fonctions propres

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -65,108 +65,90 @@ def copy_x_seconds (wav_data_src, wav_data_dst, nb_seconds, sample_rate, cur_pos
     
     return src_eof, nb_beats, new_cur_pos
 
-def usage ():
-    print "dataset.py <wav path> <number of seconds> <frame size>"
+def wav_separation (wav_path, nb_seconds, frame_size):
+    #open src wav file
+    [wav_data, sample_rate] = librosa.load(wav_path)
 
-#main
-if __name__ == '__main__':
-    import sys
-    import os
+    #info on wav separation :
+    #wav_eof = end of source file,
+    #nb_subfiles = number of smaller wav files that were created
+    #list_nb_beats = list that keeps track of the number of beats played in each subfile
+    #cur_pos = position pointer in the data of wav data
+    wav_eof = False
+    nb_subfiles = 0
+    list_nb_beats = []
+    cur_pos = 0
 
-    if(len(sys.argv) < 3):
-        usage()
-
-    else:
-        wav_path = sys.argv[1]
-        mid_path = os.path.splitext(wav_path)[0] + ".mid"
-
-        nb_seconds = int(sys.argv[2])
-        frame_size = int(sys.argv[3])
-
-        #open src wav file
-        [wav_data, sample_rate] = librosa.load(wav_path)
-
-        #info on wav separation :
-        #wav_eof = end of source file,
-        #nb_subfiles = number of smaller wav files that were created
-        #list_nb_beats = list that keeps track of the number of beats played in each subfile
-        #cur_pos = position pointer in the data of wav data
-        wav_eof = False
-        nb_subfiles = 0
-        list_nb_beats = []
-        cur_pos = 0
-
-        #wav separation
-        while (not wav_eof):
-            tmp_wav_path = os.path.splitext(wav_path)[0] + "_" + str(nb_subfiles) + ".wav"
-
-            tmp_wav = []
+    #wav separation
+    while (not wav_eof):
+        tmp_wav_path = os.path.splitext(wav_path)[0] + "_" + str(nb_subfiles) + ".wav"
+        tmp_wav = []
             
-            [wav_eof, nb_beats, cur_pos] = copy_x_seconds (wav_data, tmp_wav, nb_seconds, sample_rate, cur_pos)
+        [wav_eof, nb_beats, cur_pos] = copy_x_seconds (wav_data, tmp_wav, nb_seconds, sample_rate, cur_pos)
 
-            list_nb_beats.append (nb_beats)
+        list_nb_beats.append (nb_beats)
             
-            #keep_reading = not wav_eof
-            if not wav_eof:
-                next_onsets = librosa.onset.onset_detect(wav_data[cur_pos:], sr=sample_rate, hop_length=frame_size)
-                if len(next_onsets) != 0 :
-                    end_of_copy_pos = cur_pos + (next_onsets[0] - 3) * frame_size
-                    tmp_wav.extend(wav_data[cur_pos : end_of_copy_pos])
-                    cur_pos = end_of_copy_pos
-                    if (cur_pos >= len(wav_data)) :
-                        wav_eof = True
-                else :
+        #keep_reading = not wav_eof
+        if not wav_eof:
+            next_onsets = librosa.onset.onset_detect(wav_data[cur_pos:], sr=sample_rate, hop_length=frame_size)
+            if len(next_onsets) != 0 :
+                end_of_copy_pos = cur_pos + (next_onsets[0] - 3) * frame_size
+                tmp_wav.extend(wav_data[cur_pos : end_of_copy_pos])
+                cur_pos = end_of_copy_pos
+                if (cur_pos >= len(wav_data)) :
                     wav_eof = True
-                librosa.output.write_wav(tmp_wav_path, np.asarray(tmp_wav), sample_rate)
+            else :
+                wav_eof = True
+            librosa.output.write_wav(tmp_wav_path, np.asarray(tmp_wav), sample_rate)
 
             nb_subfiles = nb_subfiles + 1
 
-        print list_nb_beats
+    return list_nb_beats
 
-        #MIDI separation following the number of notes counted in each wav
-        nb_subfiles = 0
-        last_tick = 0
-        cur_tick = 0
-        id_cur_event = 0
-        midi_eot = False
+def midi_separation (mid_path, list_nb_beats):
+    nb_subfiles = 0
+    last_tick = 0
+    cur_tick = 0
+    id_cur_event = 0
+    midi_eot = False
 
-        #open src MIDI file
-        mid_data = midi.read_midifile(mid_path)
-        mid_data.make_ticks_abs()
+    #open src MIDI file
+    mid_data = midi.read_midifile(mid_path)
+    mid_data.make_ticks_abs()
         
-        metadata_track, src_notes_track = copy_metadata (mid_data)
+    metadata_track, src_notes_track = copy_metadata (mid_data)
 
-        nb_note_events = len(src_notes_track)
+    nb_note_events = len(src_notes_track)
                 
-        for nb_beats in list_nb_beats :
-            last_tick = 0
-            tmp_pattern = midi.Pattern()
-            tmp_pattern.resolution = mid_data.resolution
-            tmp_mid_path = os.path.splitext(wav_path)[0] + "_" + str(nb_subfiles) + ".mid"
-            nb_subfiles = nb_subfiles + 1
+    for nb_beats in list_nb_beats :
+        last_tick = 0
+        tmp_pattern = midi.Pattern()
+        tmp_pattern.resolution = mid_data.resolution
+        tmp_mid_path = os.path.splitext(mid_path)[0] + "_" + str(nb_subfiles) + ".mid"
+        nb_subfiles = nb_subfiles + 1
 
-            tmp_pattern.append(metadata_track)
+        tmp_pattern.append(metadata_track)
 
-            notes_track = midi.Track()
+        notes_track = midi.Track()
 
-            nb_beats_copied = 0
+        nb_beats_copied = 0
 
-            saw_first_onset = False
+        saw_first_onset = False
 
-            #main MIDI segmentation loop
-            while (nb_beats_copied != nb_beats):
-                cur_event = src_notes_track[id_cur_event]
-                cur_tick = cur_event.tick
+        #main MIDI segmentation loop
+        while (nb_beats_copied != nb_beats):
+            cur_event = src_notes_track[id_cur_event]
+            cur_tick = cur_event.tick
 
-                if type(cur_event) == midi.events.NoteOnEvent\
-                  or type(cur_event) == midi.events.NoteOffEvent:
+            if type(cur_event) == midi.events.NoteOnEvent\
+                or type(cur_event) == midi.events.NoteOffEvent:
 
-                  if (not saw_first_onset)\
-                    and type(cur_event) == midi.events.NoteOnEvent \
-                    and cur_event.velocity != 0:
+                if (not saw_first_onset)\
+                  and type(cur_event) == midi.events.NoteOnEvent \
+                  and cur_event.velocity != 0:
                     saw_first_onset = True
 
-                  if saw_first_onset:
+                if saw_first_onset:
                     cur_event.tick = cur_tick - last_tick
                     notes_track.append(cur_event)
                     
@@ -186,31 +168,56 @@ if __name__ == '__main__':
                           look_ahead_event = src_notes_track[id_cur_event]
                         id_cur_event = id_cur_event - 1
                       
-                last_tick = cur_tick
+            last_tick = cur_tick
 
-                id_cur_event = id_cur_event + 1
-                if id_cur_event >= nb_note_events:
-                    print "Reached end of track"
-                    midi_eot = True
-                    break
-                
-                if (nb_beats_copied == nb_beats):
-                    i = 1
-                    look_ahead_event = src_notes_track[id_cur_event + i]
-                    while not ((type(look_ahead_event) == midi.events.NoteOnEvent\
-                                    or type(look_ahead_event) == midi.events.NoteOffEvent)\
-                                   and look_ahead_event.pitch == cur_event.pitch \
-                                   and look_ahead_event.velocity == 0):
-                        look_ahead_event = src_notes_track[id_cur_event + i]
-                        i = i + 1
-                        last_tick = look_ahead_event.tick - cur_tick
-                        if id_cur_event + i >= len(src_notes_track):
-                            break
-
-            notes_track.append(midi.EndOfTrackEvent(tick=last_tick))
-            tmp_pattern.append(notes_track)
-
-            midi.write_midifile (tmp_mid_path, tmp_pattern)
-
-            if midi_eot :
+            id_cur_event = id_cur_event + 1
+            if id_cur_event >= nb_note_events:
+                print "Reached end of track"
+                midi_eot = True
                 break
+                
+            if (nb_beats_copied == nb_beats):
+                i = 1
+                look_ahead_event = src_notes_track[id_cur_event + i]
+                while not ((type(look_ahead_event) == midi.events.NoteOnEvent\
+                                or type(look_ahead_event) == midi.events.NoteOffEvent)\
+                                and look_ahead_event.pitch == cur_event.pitch \
+                                and look_ahead_event.velocity == 0):
+                    look_ahead_event = src_notes_track[id_cur_event + i]
+                    i = i + 1
+                    last_tick = look_ahead_event.tick - cur_tick
+                    if id_cur_event + i >= len(src_notes_track):
+                        break
+
+        notes_track.append(midi.EndOfTrackEvent(tick=last_tick))
+        tmp_pattern.append(notes_track)
+
+        midi.write_midifile (tmp_mid_path, tmp_pattern)
+
+        if midi_eot :
+            break
+
+def usage ():
+    print "dataset.py <wav path> <number of seconds> <frame size>"
+
+#main
+if __name__ == '__main__':
+    import sys
+    import os
+
+    if(len(sys.argv) < 3):
+        usage()
+
+    else:
+        wav_path = sys.argv[1]
+        mid_path = os.path.splitext(wav_path)[0] + ".mid"
+
+        nb_seconds = int(sys.argv[2])
+        frame_size = int(sys.argv[3])
+
+        #separate the sub-wavs
+        list_nb_beats = wav_separation (wav_path, nb_seconds, frame_size)
+        print list_nb_beats
+
+        #MIDI separation following the number of notes counted in each wav
+        midi_separation (mid_path, list_nb_beats)
